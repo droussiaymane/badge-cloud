14cdca620dbe59adfe506a471fa9c77bd2b9beeb
droussiaymane <aymanedroussi.off@gmail.com>
2023-10-18 15:35:58 +0100

    first commit

--- backend/digital-wallet-service/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user_category_model (id, category) VALUES
-                                                      (1, 'Administrator'),
-                                                      (2, 'Student'),
-                                                      (3, 'Instructor');
